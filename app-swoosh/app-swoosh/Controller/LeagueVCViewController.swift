ba4ea22663c9576c824817d8f84006a987d8fcfd
Nico Yeager <nicoyeager@gmail.com>
2020-04-05 19:05:12 -0400

    misc lesson changes + segues progr

--- app-swoosh/app-swoosh/Controller/LeagueVCViewController.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVCViewController.swift
@@ -9,13 +9,17 @@
 import UIKit
 
 class LeagueVCViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    } 
 
     /*
     // MARK: - Navigation
